b476a17765a427bc8d15bbdc2870be526444bfbc
luanalesnioski <145451858+luanalesnioski@users.noreply.github.com>
2025-03-12 08:07:52 -0300

    7

--- script.js
+++ script.js
@@ -32,4 +32,7 @@ alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
 })
 
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
