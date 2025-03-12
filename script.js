36d7108d97d7309cd4d4efd1b076a24706a8b529
luanalesnioski <145451858+luanalesnioski@users.noreply.github.com>
2025-03-12 08:00:23 -0300

    6

--- script.js
+++ script.js
@@ -26,4 +26,10 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 })
 
+const alternaContraste = document.getElementById('alterna-contraste')
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+
 
